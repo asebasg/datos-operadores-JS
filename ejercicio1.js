b732de7a7adfd9a054223b0f02b5723827952cf1
Sebastian Ospina <asebasg07@gmail.com>
2025-05-06 16:21:55 -0500

    Ejercicio 1

--- ejercicio1.js
+++ ejercicio1.js
@@ -1,12 +1,14 @@
 const DIAS_ANIO = 365;
-const cantCaracteres = 5;
 
-let nombre = prompt("Bienvenido, ingresa tu nombre a continnuación");
+let cantCaracteres = 5;
+var nombre = null;
+var nombre = prompt("Bienvenido, ingresa tu nombre a continnuación");
 let edadStr = prompt("Ahora ingresa tu edad");
 let edad = parseInt(edadStr); // Convertir la edad a un número entero
+let caracteresNombre = nombre.length < 5; // Se verifica si el nombre tiene menos de 5 caracteres
+let edadEnDias = edad * DIAS_ANIO; // Aqui se calcula la edad en dias
 
-if (nombre.length === cantCaracteres) {
-    console.log(nombre + ", tu nombre tiene 5 letras, ¡genial!");
-} else {
-    console.log("Tu nombre tiene mas de 5 caracteres");
-}
+console.log("Hola, " + nombre + "... Te dire una cosa...");
+console.log("Tu edad en días es: " + edadEnDias + " días.");
+console.log("Tu nombre tiene menos de 5 caracteres: " + caracteresNombre);
+console.log((nombre = null));
